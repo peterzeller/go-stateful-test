4e8c1cb914cb133346aa978cfa001d09c4587857
Peter Zeller <p_zeller@cs.uni-kl.de>
2022-04-02 12:16:28 +0200

    add assertions to shrinking test

--- quickcheck/shrink_test.go
+++ quickcheck/shrink_test.go
@@ -5,17 +5,30 @@ import (
 
 	"github.com/peterzeller/go-fun/iterable"
 	"github.com/peterzeller/go-fun/linkedlist"
+	"github.com/stretchr/testify/require"
 )
+
+// toSlices you say?
+func toSlices[T any, I iterable.Iterable[T]](l iterable.Iterable[I]) [][]T {
+	return iterable.ToSlice(
+		iterable.Map(func(i I) []T {
+			return iterable.ToSlice[T](i)
+		})(l))
+}
 
 func TestRemoves(t *testing.T) {
 	list := linkedlist.New(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
-	shrinks := removes(5, 10, list)
+	require.Equal(t, [][]int{
+		{1, 2, 3, 4, 5},
+		{6, 7, 8, 9, 10}}, toSlices[int](removes(5, 10, list)))
 
-	t.Logf("original = %+v", list)
-	for it := iterable.Start(shrinks); it.HasNext(); it.Next() {
-		t.Logf("list = %v", it.Current())
-	}
+	require.Equal(t, [][]int{
+		{1, 2, 3, 4, 5, 6, 7, 8},
+		{1, 2, 3, 4, 5, 6, 9, 10},
+		{1, 2, 3, 4, 7, 8, 9, 10},
+		{1, 2, 5, 6, 7, 8, 9, 10},
+		{3, 4, 5, 6, 7, 8, 9, 10}}, toSlices[int](removes(2, 10, list)))
 }
 
 func TestShrinkList(t *testing.T) {
@@ -25,9 +38,38 @@ func TestShrinkList(t *testing.T) {
 		return iterable.Singleton(t / 2)
 	})
 
-	t.Logf("original = %+v", list)
-	for it := iterable.Start(shrinks); it.HasNext(); it.Next() {
-		t.Logf("list = %v", it.Current())
-	}
-
+	require.Equal(t, [][]int{
+		// remove all 10
+		{},
+		// remove 5
+		{1, 2, 3, 4, 5},
+		{6, 7, 8, 9, 10},
+		// remove 2
+		{1, 2, 3, 4, 5, 6, 7, 8},
+		{1, 2, 3, 4, 5, 6, 9, 10},
+		{1, 2, 3, 4, 7, 8, 9, 10},
+		{1, 2, 5, 6, 7, 8, 9, 10},
+		{3, 4, 5, 6, 7, 8, 9, 10},
+		// remove 1
+		{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		{1, 2, 3, 4, 5, 6, 7, 8, 10},
+		{1, 2, 3, 4, 5, 6, 7, 9, 10},
+		{1, 2, 3, 4, 5, 6, 8, 9, 10},
+		{1, 2, 3, 4, 5, 7, 8, 9, 10},
+		{1, 2, 3, 4, 6, 7, 8, 9, 10},
+		{1, 2, 3, 5, 6, 7, 8, 9, 10},
+		{1, 2, 4, 5, 6, 7, 8, 9, 10},
+		{1, 3, 4, 5, 6, 7, 8, 9, 10},
+		{2, 3, 4, 5, 6, 7, 8, 9, 10},
+		// shrink one element
+		{0, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		{1, 1, 3, 4, 5, 6, 7, 8, 9, 10},
+		{1, 2, 1, 4, 5, 6, 7, 8, 9, 10},
+		{1, 2, 3, 2, 5, 6, 7, 8, 9, 10},
+		{1, 2, 3, 4, 2, 6, 7, 8, 9, 10},
+		{1, 2, 3, 4, 5, 3, 7, 8, 9, 10},
+		{1, 2, 3, 4, 5, 6, 3, 8, 9, 10},
+		{1, 2, 3, 4, 5, 6, 7, 4, 9, 10},
+		{1, 2, 3, 4, 5, 6, 7, 8, 4, 10},
+		{1, 2, 3, 4, 5, 6, 7, 8, 9, 5}}, toSlices[int](shrinks))
 }
